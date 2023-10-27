3c69cf977f82218f595cc55b48d8aa587e7035b6
greenlindsay <linds021@byui.edu>
2023-10-26 23:51:41 -0600

    l

---
+++ lessons/lesson07/scripts/getdates.js
@@ -0,0 +1,3 @@
+document.getElementById("year").innerHTML = new Date().getFullYear();
+let mod = document.lastModified;
+document.getElementById("current").textContent = mod
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,18 @@
 document.getElementById("year").innerHTML = new Date().getFullYear();
 let mod = document.lastModified;
 document.getElementById("current").textContent = mod
+
+const showVisits = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	showVisits.textContent = numVisits;
+} else {
+	showVisits.textContent = `This is your first visit!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
