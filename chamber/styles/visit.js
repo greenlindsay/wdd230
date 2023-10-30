843de092ff268815a09f106e5deb8029ca2602e2
greenlindsay <linds021@byui.edu>
2023-10-29 21:33:57 -0600

    f

---
+++ chamber/styles/visit.js
@@ -0,0 +1,13 @@
+const showVisits = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	showVisits.textContent = numVisits;
+} else {
+	showVisits.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
