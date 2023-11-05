fd187a64d8e028eac2b3f5117b8040f7e09a5cb9
greenlindsay <linds021@byui.edu>
2023-11-04 18:53:10 -0600

    jm

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -1,2 +1,5 @@
 let mod = document.lastModified;
 document.getElementById("current").textContent = mod
+
+let tmstamp = document.lastModified;
+document.getElementById("stamp").textContent = tmstamp
