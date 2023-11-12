6ec521252ddb9e373d58d301a3419bbc65052f2d
greenlindsay <linds021@byui.edu>
2023-11-11 23:26:51 -0700

    ,

--- chamber/scripts/getdates.js
+++ chamber/scripts/getdates.js
@@ -3,3 +3,20 @@ document.getElementById("current").textContent = mod
 
 let tmstamp = document.lastModified;
 document.getElementById("stamp").textContent = tmstamp
+
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+
+gridbutton.addEventListener("click", () => {
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", () => {
+	display.classList.add("list");
+	display.classList.remove("grid");
+});
