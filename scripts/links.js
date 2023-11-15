b01426423aace26cd67f52f7b5c1f024ff0e55bb
greenlindsay <linds021@byui.edu>
2023-11-14 23:16:36 -0700

    p

--- scripts/links.js
+++ scripts/links.js
@@ -4,6 +4,5 @@ const linksURL="https://greenlindsay.github.io/wdd230/data/links.json";
 async function getLinks(){
     const response = await fetch(url);
     const links = await response.json();
-    console.log(data)
-    
-}
+    console.log(data);
+}   
