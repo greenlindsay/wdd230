c519fa296d9818df398f2003af55cc81c18848d6
greenlindsay <linds021@byui.edu>
2023-11-14 23:49:33 -0700

    l

--- scripts/links.js
+++ scripts/links.js
@@ -1,8 +1,23 @@
 const baseURL="https://greenlindsay.github.io/wdd230/";
 const linksURL="https://greenlindsay.github.io/wdd230/data/links.json";
-
+const links = document.querySelector('#links');
 async function getLinks(){
     const response = await fetch(url);
     const links = await response.json();
     console.log(data);
+
+    displayLinks(data);
 }   
+
+const displayLinks=(weeks)=>{
+    weeks.forEach(week => {
+        let li = document.createElement('li')
+        let link = document.createElement('a');
+        link.textContent = week.title;
+
+        link.setAttribute('href',week.)
+        let title = document.createElement('h1')
+    });
+}
+
+getLinks();
