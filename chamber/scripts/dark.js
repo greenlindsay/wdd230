7cf9fe6db08259cd0aa336dd746f1a03158c47b0
greenlindsay <linds021@byui.edu>
2023-11-25 23:29:19 -0700

    E

--- chamber/scripts/dark.js
+++ chamber/scripts/dark.js
@@ -1,24 +1,8 @@
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "🔆";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "🕶️";
-	}
-});
 
-const actNav = document.querySelector('nav');
-const actA = document.querySelector('a');
-
-actA.forEach(navActive => {
-	navActive.addEventListener('click', function(){
-		actNav.querySelector('.active').classList.remove('active')
-		navActive.classList.add('active')
-	})
-})
+function darkMode(){
+	var dark = document.body;
+	Element.classList.toggle("dark-mode");
+}
